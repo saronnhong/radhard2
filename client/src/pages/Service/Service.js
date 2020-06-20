0aa10648c2ccb06c9216a2eb5551cc4392fd0bf9
Saron <saronnhong@gmail.com>
2020-06-19 17:30:38 -0700

    fix service page view in mobile

--- client/src/pages/Service/Service.js
+++ client/src/pages/Service/Service.js
@@ -18,24 +18,16 @@ class Service extends Component {
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/flying-sat-min.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">RADIATION HARDENING BY PROCESS AND DESIGN</h2>
+                            <p className="serviceListText">ASICs, FPGAs, digital, analog, and mixed component & process design for radiation-hardened or radiation tolerant space missions.</p>
                         </div>
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/COTS-min.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">COTS PARTS MISSION ASSURANCE</h2>
-                        </div>
-                        <div className="col-md-4">
-                            <img className="serviceListImage" src="/images/TCAD-min.jpg" width="95%" alt="service_image" />
-                            <h2 className="serviceListTitle">TCAD DESIGN & SIMULATIONS</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <p className="serviceListText">ASICs, FPGAs, digital, analog, and mixed component & process design for radiation-hardened or radiation tolerant space missions.</p>
-                        </div>
-                        <div className="col-md-4">
                             <p className="serviceListText">Commercial Off The Shelf (COTS) component assessment for use in radiation environment.</p>
                         </div>
                         <div className="col-md-4">
+                            <img className="serviceListImage" src="/images/TCAD-min.jpg" width="95%" alt="service_image" />
+                            <h2 className="serviceListTitle">TCAD DESIGN AND SIMULATIONS</h2>
                             <p className="serviceListText">Technology Computer Aided Design (TCAD) modeling and analysis, including device operation in radiation environment.</p>
                         </div>
                     </div>
@@ -43,42 +35,30 @@ class Service extends Component {
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/EDA-Flow-min.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">EDA FLOWS</h2>
+                            <p className="serviceListText">Electronic Design Automation (EDA) flow for complex integrated circuit designs.</p>
                         </div>
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/circuit-board2-min.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">RADIATION HARDENED PDK</h2>
+                            <p className="serviceListText">Process Design Kit (PDK) development and assessment for radiation hardened designs.</p>
                         </div>
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/component-analysis-min.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">COMPONENT ANALYSES</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <p className="serviceListText">Electronic Design Automation (EDA) flow for complex integrated circuit designs.</p>
-                        </div>
-                        <div className="col-md-4">
-                            <p className="serviceListText">Process Design Kit (PDK) development and assessment for radiation hardened designs.</p>
-                        </div>
-                        <div className="col-md-4">
                             <p className="serviceListText">Single event rate predictions, total ionizing and non-ionizing dose impact.</p>
+
                         </div>
                     </div>
                     <div className="row serviceRow2">
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/Radiation-Hardening.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">RADIATION INDUCED UPSET MITIGATION</h2>
+                            <p className="serviceListText">Mitigation techniques for radiation-induced upsets in devices and circuits.</p>
+
                         </div>
                         <div className="col-md-4">
                             <img className="serviceListImage" src="/images/Radiation-Testing-min.jpg" width="95%" alt="service_image" />
                             <h2 className="serviceListTitle">RADIATION TESTING</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <p className="serviceListText">Mitigation techniques for radiation-induced upsets in devices and circuits.</p>
-                        </div>
-                        <div className="col-md-4">
                             <p className="serviceListText">Heavy ion, proton, neutron, and gamma ray radiation test preparation, execution, and reporting.</p>
                         </div>
                     </div>
